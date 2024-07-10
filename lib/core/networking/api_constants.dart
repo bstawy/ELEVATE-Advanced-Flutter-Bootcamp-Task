b75cafe156c790ca5c1b452fb082d24cb365012f
bastawy <125200508@sha.edu.eg>
2024-07-10 17:01:20 +0300

    Fix slashes in api constants

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String baseUrl = 'https://dummyjson.com/';
-  static const String productsEndPoint = "products";
+  static const String baseUrl = 'https://dummyjson.com';
+  static const String productsEndPoint = "/products";
 }
