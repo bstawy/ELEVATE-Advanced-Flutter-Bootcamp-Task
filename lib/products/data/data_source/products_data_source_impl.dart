20aee6e435537a56d8fd01a701c47d885d1a44d6
bastawy <125200508@sha.edu.eg>
2024-07-10 22:15:26 +0300

    chore: Update Dio dependency and refactor ProductsDataSourceImpl

--- lib/products/data/data_source/products_data_source_impl.dart
+++ lib/products/data/data_source/products_data_source_impl.dart
@@ -2,14 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../core/networking/api_constants.dart';
+import '../../../core/networking/dio_factory.dart';
 import 'products_data_source.dart';
 
 @Injectable(as: ProductsDataSource)
 class ProductsDataSourceImpl implements ProductsDataSource {
-  final Dio _dio;
+  late Dio _dio;
 
-  @factoryMethod
-  ProductsDataSourceImpl(this._dio);
+  ProductsDataSourceImpl() {
+    _dio = DioFactory.getDio();
+  }
 
   @override
   Future<Response> getProducts() async {
