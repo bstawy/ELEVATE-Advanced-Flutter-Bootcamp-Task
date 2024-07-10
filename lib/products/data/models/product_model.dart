991ff9942e13fcfe4bb989d88c49dd53bb330fe4
bastawy <125200508@sha.edu.eg>
2024-07-10 17:02:52 +0300

    Create product data model which extends from product entity

---
+++ lib/products/data/models/product_model.dart
@@ -0,0 +1,56 @@
+import '../../domain/entities/product_entity.dart';
+
+class ProductModel extends ProductEntity {
+  ProductModel({
+    required super.id,
+    required super.title,
+    required super.description,
+    required super.category,
+    required super.price,
+    required super.discountPercentage,
+    required super.rating,
+    required super.stock,
+    required super.tags,
+    required super.brand,
+    required super.sku,
+    required super.weight,
+    required super.dimensions,
+    required super.warrantyInformation,
+    required super.shippingInformation,
+    required super.availabilityStatus,
+    required super.reviews,
+    required super.returnPolicy,
+    required super.minimumOrderQuantity,
+    required super.meta,
+    required super.images,
+    required super.thumbnail,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      category: json['category'],
+      price: json['price'],
+      discountPercentage: json['discountPercentage'],
+      rating: json['rating'],
+      stock: json['stock'],
+      tags: List.castFrom<dynamic, String>(json['tags']),
+      brand: json['brand'],
+      sku: json['sku'],
+      weight: json['weight'],
+      dimensions: Dimensions.fromJson(json['dimensions']),
+      warrantyInformation: json['warrantyInformation'],
+      shippingInformation: json['shippingInformation'],
+      availabilityStatus: json['availabilityStatus'],
+      reviews:
+          List.from(json['reviews']).map((e) => Reviews.fromJson(e)).toList(),
+      returnPolicy: json['returnPolicy'],
+      minimumOrderQuantity: json['minimumOrderQuantity'],
+      meta: Meta.fromJson(json['meta']),
+      images: List.castFrom<dynamic, String>(json['images']),
+      thumbnail: json['thumbnail'],
+    );
+  }
+}
