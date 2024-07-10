da4e122a87ddccea80ca9e014cb19e36b820602e
bastawy <125200508@sha.edu.eg>
2024-07-10 17:02:32 +0300

    Import new named product entity

--- lib/products/domain/repository/products_repository.dart
+++ lib/products/domain/repository/products_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../product_model.dart';
+import '../entities/product_entity.dart';
 
 abstract class ProductsRepository {
   Future<Either<String, List<ProductEntity>>> getProducts();
