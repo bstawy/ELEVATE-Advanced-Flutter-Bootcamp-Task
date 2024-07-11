5179639aaf187d2928a431b25be4c83d03048dc3
bastawy <125200508@sha.edu.eg>
2024-07-11 15:57:16 +0300

    Remove unused properties from ProductEntity and ProductModel

--- lib/products/domain/entities/product_entity.dart
+++ lib/products/domain/entities/product_entity.dart
@@ -8,21 +8,10 @@ class ProductEntity extends Equatable {
   final num? price;
   final num? discountPercentage;
   final num? rating;
-  final num? stock;
-  final List<dynamic>? tags;
   final String? brand;
-  final String? sku;
-  final num? weight;
-  final Dimensions? dimensions;
-  final String? warrantyInformation;
-  final String? shippingInformation;
-  final String? availabilityStatus;
-  final List<Reviews>? reviews;
-  final String? returnPolicy;
-  final num? minimumOrderQuantity;
-  final Meta? meta;
   final List<dynamic>? images;
   final String? thumbnail;
+  final bool isFavorite;
 
   const ProductEntity({
     this.id,
@@ -32,21 +21,10 @@ class ProductEntity extends Equatable {
     this.price,
     this.discountPercentage,
     this.rating,
-    this.stock,
-    this.tags,
     this.brand,
-    this.sku,
-    this.weight,
-    this.dimensions,
-    this.warrantyInformation,
-    this.shippingInformation,
-    this.availabilityStatus,
-    this.reviews,
-    this.returnPolicy,
-    this.minimumOrderQuantity,
-    this.meta,
     this.images,
     this.thumbnail,
+    this.isFavorite = false,
   });
 
   factory ProductEntity.fromJson(Map<String, dynamic> json) {
@@ -58,20 +36,7 @@ class ProductEntity extends Equatable {
       price: json['price'],
       discountPercentage: json['discountPercentage'],
       rating: json['rating'],
-      stock: json['stock'],
-      tags: (json['tags'] as List),
       brand: json['brand'],
-      sku: json['sku'],
-      weight: json['weight'],
-      dimensions: Dimensions.fromJson(json['dimensions']),
-      warrantyInformation: json['warrantyInformation'],
-      shippingInformation: json['shippingInformation'],
-      availabilityStatus: json['availabilityStatus'],
-      reviews:
-          (json['reviews'] as List).map((e) => Reviews.fromJson(e)).toList(),
-      returnPolicy: json['returnPolicy'],
-      minimumOrderQuantity: json['minimumOrderQuantity'],
-      meta: Meta.fromJson(json['meta']),
       images: (json['images'] as List),
       thumbnail: json['thumbnail'],
     );
@@ -86,83 +51,9 @@ class ProductEntity extends Equatable {
         price,
         discountPercentage,
         rating,
-        stock,
-        tags,
         brand,
-        sku,
-        weight,
-        dimensions,
-        warrantyInformation,
-        shippingInformation,
-        availabilityStatus,
-        reviews,
-        returnPolicy,
-        minimumOrderQuantity,
-        meta,
         images,
         thumbnail,
+        isFavorite,
       ];
-}
-
-class Dimensions {
-  final num? width;
-  final num? height;
-  final num? depth;
-
-  Dimensions({
-    this.width,
-    this.height,
-    this.depth,
-  });
-
-  factory Dimensions.fromJson(Map<String, dynamic> json) {
-    return Dimensions(
-      width: json['width'],
-      height: json['height'],
-      depth: json['depth'],
-    );
-  }
-}
-
-class Reviews {
-  final num? rating;
-  final String? comment;
-  final String? date;
-  final String? reviewerName;
-  final String? reviewerEmail;
-
-  Reviews({
-    this.rating,
-    this.comment,
-    this.date,
-    this.reviewerName,
-    this.reviewerEmail,
-  });
-
-  factory Reviews.fromJson(Map<String, dynamic> json) {
-    return Reviews(
-      rating: json['rating'],
-      comment: json['comment'],
-      date: json['date'],
-      reviewerName: json['reviewerName'],
-      reviewerEmail: json['reviewerEmail'],
-    );
-  }
-}
-
-class Meta {
-  final String? barcode;
-  final String? qrCode;
-
-  Meta({
-    this.barcode,
-    this.qrCode,
-  });
-
-  factory Meta.fromJson(Map<String, dynamic> json) {
-    return Meta(
-      barcode: json['barcode'],
-      qrCode: json['qrCode'],
-    );
-  }
 }
