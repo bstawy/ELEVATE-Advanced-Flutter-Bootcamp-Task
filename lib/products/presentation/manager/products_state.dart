8d5c6b8ef97797fa7b3d7b1dc07f4efa1fb4216b
bastawy <125200508@sha.edu.eg>
2024-07-13 01:14:32 +0300

    Make products list public and make product state extends equatable to ensure equality

--- lib/products/presentation/manager/products_state.dart
+++ lib/products/presentation/manager/products_state.dart
@@ -1,20 +1,32 @@
 part of 'products_cubit.dart';
 
 @immutable
-sealed class ProductsState {}
+sealed class ProductsState extends Equatable {}
 
-final class ProductsInitialState extends ProductsState {}
+final class ProductsInitialState extends ProductsState {
+  @override
+  List<Object?> get props => [];
+}
 
-final class ProductsLoadingState extends ProductsState {}
+final class ProductsLoadingState extends ProductsState {
+  @override
+  List<Object?> get props => [];
+}
 
 final class ProductsLoadedState extends ProductsState {
   final List<ProductEntity> products;
 
   ProductsLoadedState(this.products);
+
+  @override
+  List<Object?> get props => [products];
 }
 
 final class ProductsErrorState extends ProductsState {
   final String message;
 
   ProductsErrorState(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
