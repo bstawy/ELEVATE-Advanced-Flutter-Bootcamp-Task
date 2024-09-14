bfb208bbe530a578f461fe1f31f84db5c2699dbc
bastawy <125200508@sha.edu.eg>
2024-09-14 22:04:11 +0300

    Refactor product image widget by adding padding to image and change background color

--- lib/products/presentation/ui/widgets/product_widget/product_image_widget.dart
+++ lib/products/presentation/ui/widgets/product_widget/product_image_widget.dart
@@ -14,12 +14,13 @@ class ProductImageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: const Color(0xFFCFCECA),
+        color: const Color(0xFFFFFFFF),
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(12.r),
           topRight: Radius.circular(12.r),
         ),
       ),
+      padding: EdgeInsets.only(top: 8.h, left: 8.w, right: 8.w),
       child: CachedNetworkImage(
         imageUrl: imageUrl,
         placeholder: (context, url) {
